1014491edbfa4075b1bdc9205ca9c6480821cbc0
LeandKa <lcalvalcanti29@gmail.com>
2021-05-27 14:33:46 -0300

    nubank

---
+++ src/pages/Options/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { Container } from '../Home/styles'
+
+export default function Options() {
+    return (
+        <Container>
+            <Text>Options</Text>
+        </Container>
+    )
+}
